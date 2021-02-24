35e2a940d3bc7ef91066865160c88ca900fe9bec
Todge <todgie16@gmail.com>
2021-02-24 14:20:09 -0500

    Addition of photos and build out of the spinner

--- Lab_3/app/src/main/java/edu/temple/lab_3/ImageActivity.java
+++ Lab_3/app/src/main/java/edu/temple/lab_3/ImageActivity.java
@@ -5,8 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import edu.temple.lab_3.ImageAdapter;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -21,14 +20,26 @@ public class ImageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView dog1 = new TextView(getApplicationContext());
-        TextView[] arraySpinner = new TextView[]{
-            dog1
-        };
+        ImageView image = findViewById(R.id.imageView);
+        TextView def = new TextView(this);
+        def.setText((CharSequence)"Select a breed:");
+        TextView dog1 = new TextView(this);
+        dog1.setText((CharSequence)"Labrador");
+        TextView dog2 = new TextView(this);
+        dog2.setText((CharSequence)"German Shepard");
+        TextView dog3 = new TextView(this);
+        dog3.setText((CharSequence)"Bulldog");
+        TextView dog4 = new TextView(this);
+        dog4.setText((CharSequence)"Rottweiler");
+
+        ArrayList<TextView> arraySpinner = new ArrayList<TextView>();
+        arraySpinner.add(dog1);
+        arraySpinner.add(dog2);
+        arraySpinner.add(dog3);
+        arraySpinner.add(dog4);
 
         Spinner s = findViewById(R.id.spinner);
         ImageAdapter adapter = new ImageAdapter(this, arraySpinner);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s.setAdapter(adapter);
     }
 }
--- Lab_3/app/src/main/java/edu/temple/lab_3/ImageAdapter.java
+++ Lab_3/app/src/main/java/edu/temple/lab_3/ImageAdapter.java
@@ -4,17 +4,20 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ImageAdapter extends BaseAdapter {
     private Context context; //context
-    private TextView[] items; //data source of the list adapter
+    private ArrayList<TextView> items; //data source of the list adapter
+    private ArrayList<ImageView> pics;
 
-    public ImageAdapter(Context context, TextView[] items) {
+    public ImageAdapter(Context context, ArrayList<TextView> items, ArrayList<ImageView> pics) {
         this.context = context;
         this.items = items;
+        this.pics = pics;
     }
 
     @Override
@@ -33,7 +36,16 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public TextView getView(int position, View convertView, ViewGroup parent) {
+        TextView contentView = new TextView(context);
+        contentView = (TextView) getItem(position);
+        contentView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        return contentView;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        convertView = pics.get(position);
         return convertView;
     }
 }
