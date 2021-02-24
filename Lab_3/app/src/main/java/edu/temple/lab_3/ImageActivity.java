3ab906034cc2f2a81f5b7089959e5f36f8e651ce
Todge <todgie16@gmail.com>
2021-02-24 11:12:43 -0500

    Custom ImageAdapter class creation

---
+++ Lab_3/app/src/main/java/edu/temple/lab_3/ImageActivity.java
@@ -0,0 +1,26 @@
+package edu.temple.lab_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.temple.lab_3.ImageAdapter;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ Lab_3/app/src/main/java/edu/temple/lab_3/ImageAdapter.java
@@ -0,0 +1,39 @@
+package edu.temple.lab_3;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context context; //context
+    private TextView[] items; //data source of the list adapter
+
+    public ImageAdapter(Context context, TextView[] items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size(); //returns total of items in the list
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position); //returns list item at the specified position
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return convertView;
+    }
+}
